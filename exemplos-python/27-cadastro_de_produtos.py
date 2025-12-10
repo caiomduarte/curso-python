888597a9dc6bbcb0725c5237dff8f5f2256ad3ce
Caio Malheiros <md.caio@yahoo.com.br>
2025-12-10 11:41:34 -0300

    Add files via upload

---
+++ exemplos-python/27-cadastro_de_produtos.py
@@ -0,0 +1,27 @@
+
+
+import os
+
+os.system("cls")
+
+
+print("=== SALVANDO DADOS DO PRODUTOS EM UM ARQUIVO ===\n")
+
+nome_produto = input("Digite o nome do produto: ")
+fabricante   = input("Digite o fabricante: ")
+categoria    = input("Digite a categoria: ")
+preco        = float(input("Digite o preço do produto:"))
+
+
+#Manipulando arquivos
+#arquivo = open("produtos.txt", "a", encoding="utf-8")
+
+with open("produtos.txt", "a", encoding="utf-8") as arquivo:
+    #Gravando as informações dentro do arquivo
+    arquivo.write(f"Produto: {nome_produto} | Fabricante: {fabricante} | Categoria: {categoria} | Preço: {preco:.2f} \n")
+    
+
+# Não é necessário fechar o arquivo quando utilizamos o with
+#arquivo.close()
+
+input("Pressione a tecla <Enter> para encerrar...")
